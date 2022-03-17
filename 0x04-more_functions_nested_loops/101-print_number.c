322a04c7529c2f347516a029ef700e67007ca1b5
ouko <oukoezzy@gmail.com>
2022-03-17 18:02:06 +0300

    more functions & nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,9 +12,14 @@ void print_number(int n){
 		_putchar('0');
     else{
         if (n < 0)
-		{
-			i = n * -1;
-			_putchar('-');
-		}
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	    if (n / 10)
+		print_number(n / 10);
+
+	    _putchar(n % 10 + '0');
     }
 }
