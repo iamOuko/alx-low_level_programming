bb8065a87c648abbf1089d9bd1381cb74cb9e55a
ouko <oukoezzy@gmail.com>
2022-03-17 12:54:27 +0300

    more functions & nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,22 +9,27 @@
  */
 
 
-int main(void){
-    int i;
+int main(void)
+{
+	int i;
 
-    for(i = 1; i <= 100; i++){
-        if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
+	for (i = 1; i <= 100; i++)
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
 		else if (i % 5 == 0)
-			printf("Buzz");
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
 		else
-			printf("%d", i);
-
-		if (i < 100)
-			printf(" ");
-    }
-    printf("\n");
+		{
+			printf("%d ", i);
+		}
+	printf("\n");
 	return (0);
 }
