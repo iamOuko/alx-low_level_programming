85cb6fbea1ff237253f0a6b8c927d372a8903a6d
ouko <oukoezzy@gmail.com>
2022-03-17 12:34:26 +0300

    more functions & nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * more_numbers - print 10 times the numbers from 0 to 14
+ * Description: Can only use _putchar 3 times
+ */
+
 void more_numbers(void){
     int i;
 	char c;
