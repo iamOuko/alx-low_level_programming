e8acf2a93dd5ca86a04ab90791bb57109f0ced4d
ouko <oukoezzy@gmail.com>
2022-03-23 16:09:30 +0300

    pointers,arrays,strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - rotate characters 13 places in the alphabet
+ * @s: string
+ * Return: string `s` rotated
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	char a[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char b[] = "nopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
+		{
+			s[i] = (s[i] - 65 > 25) ?
+				b[s[i] - 97] : a[s[i] - 65];
+		}
+	}
+	return (s);
+}
