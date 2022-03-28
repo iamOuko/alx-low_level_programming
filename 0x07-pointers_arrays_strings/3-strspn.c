c37191bf754ebd81a611fca8f346b19e2f2d2eae
ouko <oukoezzy@gmail.com>
2022-03-28 16:30:18 +0300

    add more pointers,arrays,strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,32 +1,36 @@
 #include "main.h"
 
 /**
- * _strspn - return length of string that c values consistently
- * @s: string to search
- * @accept: target c
- * Return: number of bytes consecutively matched
+ * _strspn - search a string for a set of bytes
+ * @s: char string array
+ * @accept: char array to check bytes with
+ * Return: Number of bytes in the intial segment of `s`
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0, j;
-	int c = 0;
+	int i;
+	int j;
+	int c;
+
+	i = 0;
+	c = 0;
 
 	while (s[i] != '\0')
 	{
-
-		for (j = 0; accept[j] != '\0'; j++) 
+		j = 0;
+		while (accept[j] != '\0')
 		{
-			if (s[i] == accept[j]) 
+			if (s[i] == accept[j])
 			{
 				c++;
 				break;
 			}
-			if (accept[j + 1] == '\0' && s[i] != accept[j])
-				return (c);
+			j++;
 		}
+		if (accept[j] == '\0')
+			break;
 		i++;
 	}
-	return (c); 
-
+	return (c);
 }
