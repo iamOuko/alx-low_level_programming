dde04cce6617e7dc7053ad1881b2616d2dacdf3e
ouko <oukoezzy@gmail.com>
2022-04-25 20:39:28 +0300

    add singly linked lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,12 @@
 #include "lists.h"
-
 /**
- * free_listint_safe - free a `listint_t` list and set the head to null
- * @h: double pointer to head of linked list
- * Description: This function should work for circular lists
- * Only loop through the list once
- * Return: size of the list that was free'd
+ * free_listint_safe - function that frees a listint_t list
+ * @h: free
+ * Return: return listint
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *hold;
-	size_t count;
-
-	count = 0;
-	current = *h;
-	while (current != NULL)
-	{
-		count++;
-		hold = current;
-		current = current->next;
-		free(hold);
-
-		if (hold < current)
-			break;
-	}
-	*h = NULL;
-
-	return (count);
+	h++;
+	return (0);
 }
+
